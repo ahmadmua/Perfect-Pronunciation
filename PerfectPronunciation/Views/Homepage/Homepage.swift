c8b53dac661cdd30d039275774f9fc1c8dc3b6f0
Jordan Bhar <48815489+JordanBhar@users.noreply.github.com>
2023-10-27 19:03:33 -0400

    Added / Updated Elements in the Homepage

--- PerfectPronunciation/Views/Homepage/Homepage.swift
+++ PerfectPronunciation/Views/Homepage/Homepage.swift
@@ -13,88 +13,95 @@ struct Homepage: View {
     @State var pronunciationPoints : Int = 0
     
     var body: some View {
-        NavigationStack{
-            VStack(alignment: .leading){
+        NavigationView { // Use NavigationView
+            ZStack{
+                Image("AppBackground")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(maxWidth: UIScreen.main.bounds.width, maxHeight: UIScreen.main.bounds.height)
+                    .edgesIgnoringSafeArea(.all)
                 
-                Text("Hello \("Name-Placehold")")
-                    .padding(.leading)
-                    .font(.headline)
-                    .bold()
-                
-                Section{
+                VStack(spacing: 20) {
+                    Text("Hello Name-Placehold")
+                        .font(.headline)
+                        .bold()
+                    
+                    
                     VStack(alignment: .leading){
                         Text("Explore")
                             .font(.largeTitle)
                             .bold()
-                            .padding(.leading)
+                        ZStack{
+                            VisualEffectView(effect: UIBlurEffect(style: .systemThinMaterialDark))
+                                .frame(width: UIScreen.main.bounds.width - 10, height: 150)
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
                             
-                        ScrollView(.horizontal, showsIndicators: false) {
-                            
-                            HStack(spacing: 20) {
-                                ForEach(data, id: \.self) { item in
-                                    VStack {
-                                        Text(item)
-                                            .font(.headline)
-                                            .padding()
-                                            .frame(width: 150, height: 75) // Adjust as needed
-                                            .background(Color.yellow)
-                                            .cornerRadius(10)
-                                            .shadow(radius: 5)
+                            VStack(alignment: .leading) {
+                                
+                                
+                                ScrollView(.horizontal, showsIndicators: false) {
+                                    HStack(spacing: 20) {
+                                        ForEach(data, id: \.self) { item in
+                                            Text(item)
+                                                .font(.headline)
+                                                .padding()
+                                                .frame(width: 150, height: 75)
+                                                .background(Color.yellow)
+                                                .cornerRadius(10)
+                                                .shadow(radius: 5)
+                                        }
                                     }
+                                    .frame(maxWidth: .infinity)
                                 }
                             }
-                            .padding()
                         }
                     }
-                }
-                
-                Section{
+                    
+                    
+                    
                     VStack(alignment: .leading){
                         Text("Goals / Stats")
                             .font(.largeTitle)
                             .bold()
-                            .padding(.leading)
-                            
-                        ScrollView(.horizontal, showsIndicators: false) {
-                            
-                            HStack(spacing: 20) {
+                        ZStack{
+                            VisualEffectView(effect: UIBlurEffect(style: .systemThinMaterialDark))
+                                .frame(width: UIScreen.main.bounds.width - 10, height: 250)
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                            VStack(alignment: .leading) {
                                 
-                                VStack(alignment: .center){
-                                    Text("Graph Place-Holder")
-                                }
-                                .frame(width: 200, height: 75)
                                 
-                                VStack(alignment: .leading, spacing: 10){
-                                    Text("Data Point 1")
-                                    
-                                    Text("Data Point 1")
-                                    
-                                    Text("Data Point 1")
-                                    
-                                    Text("Data Point 1")
-                                    
+                                ScrollView(.horizontal, showsIndicators: false) {
+                                    HStack(spacing: 20) {
+                                        Text("Graph Place-Holder")
+                                            .frame(width: 200, height: 75)
+                                        
+                                        VStack(alignment: .center, spacing: 10){
+                                            Text("Data Point 1")
+                                            Text("Data Point 1")
+                                            Text("Data Point 1")
+                                            Text("Data Point 1")
+                                        }
+                                    }
+                                    .frame(maxWidth: .infinity)
                                 }
-                                .padding(.leading)
                             }
-                            .padding()
                         }
                     }
-                }
-            
-            }
-            .padding(.top, 20)
-            .navigationBarItems(leading:
-                                    Image("Alpaca")
-                                        .resizable()
-                                        .padding(4)
-                                        .frame(width: 45, height: 40)
-                                        .clipShape(Circle())
-                                        .background(Circle().fill(Color.yellow))
-                                        .overlay(Circle().stroke(Color.black, lineWidth: 2))
-            )
-            .navigationBarItems(trailing:
-                HStack(spacing: 20){
                     
+                   
+                }
+                .padding(.top, 20)
+                .padding(.horizontal, 20)
+                .navigationBarItems(leading:
+                                        Image("Alpaca")
+                    .resizable()
+                    .padding(4)
+                    .frame(width: 45, height: 40)
+                    .clipShape(Circle())
+                    .background(Circle().fill(Color.yellow))
+                    .overlay(Circle().stroke(Color.black, lineWidth: 2)),
+                                    trailing:
+                                        HStack(spacing: 20){
                     HStack{
                         Image(systemName: "music.mic.circle")
                             .resizable()
@@ -104,27 +111,21 @@ struct Homepage: View {
                         Text(" \(String(pronunciationPoints)) pts")
                             .foregroundColor(.black)
                     }
-                        .foregroundColor(.white)
-                        .padding(.horizontal, 25)
-                        .padding(.vertical, 6)
-                        .background(RoundedRectangle(cornerRadius: 30)
-                                        .fill(Color.yellow))
-                        .overlay(RoundedRectangle(cornerRadius: 30).stroke(Color.black, lineWidth: 2))
-                
+                    .padding(.horizontal, 25)
+                    .padding(.vertical, 6)
+                    .background(RoundedRectangle(cornerRadius: 30).fill(Color.yellow))
+                    .overlay(RoundedRectangle(cornerRadius: 30).stroke(Color.black, lineWidth: 2))
+                    
                     Image(systemName: "person.circle")
                         .resizable()
                         .frame(width: 40, height: 40)
                         .foregroundColor(.black)
                         .background(Circle().fill(Color.yellow))
-                        //.overlay(Circle().stroke(Color.black, lineWidth: 2))
-
-                    
                 }
-                                
-            )
-            
-            Spacer()
-            
+                )
+                
+                Spacer()
+            }
         }
         .navigationBarBackButtonHidden(true)
         
